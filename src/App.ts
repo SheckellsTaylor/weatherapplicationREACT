dafdc91e0d44a4cfbe2e53801693761061cda9d3
Barrett Bolivar <barrettbolivar@gmail.com>
2019-01-24 19:59:28 -0500

    Creation of some components

---
+++ src/App.ts
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Banner from './landingPage/banner/banner';
+
+class App extends Component {
+  render() {
+    return (
+      <Banner/>
+    );
+  }
+}
+
+export default App;
